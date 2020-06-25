197810989133f6f0afaf5713dc3eee3029d0c771
mraees93 <53144654+mraees93@users.noreply.github.com>
2020-06-25 14:39:06 +0200

    Updates

---
+++ precache-manifest.3f9b29f10c56eff39e336073930c0da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd9f0f12658e02b43e6a58ba1659509",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "fdc61ab75ac4235b4ab5",
+    "url": "/recipe-app/static/css/main.02798154.chunk.css"
+  },
+  {
+    "revision": "86c2b469b09b71291da2",
+    "url": "/recipe-app/static/js/2.97b8a675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.97b8a675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc61ab75ac4235b4ab5",
+    "url": "/recipe-app/static/js/main.674e8e83.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
